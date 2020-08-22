872ecfada30378b8d37ce7ef1655c190415abd3a
akshaygoel007 <akshaygoel534@gmail.com>
2020-08-22 09:52:52 +0530

    first commit

---
+++ db/access.php
@@ -0,0 +1,26 @@
+<?php
+    $capabilities = array(
+ 
+    'block/v7_podcast:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+ 
+    'block/v7_podcast:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+ 
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
